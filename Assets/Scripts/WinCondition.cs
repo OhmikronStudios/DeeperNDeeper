62caa1f545aae3af32e3ed9c2cdb1b56c224a750
RyanSalam <Ryan@agni.com>
2021-04-24 15:40:44 -0400

    Dotween Import and Level renamed level

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Cinemachine;
+
+public class WinCondition : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera winCam;
+
+    [SerializeField] private float timeToBeat = 10.0f;
+    [SerializeField] GameObject GameWinUI;
+
+    private float timeElapsed;
+    private bool hasPlayerDied = false;
+
+    [SerializeField] private GameObject ball;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            winCam.Priority = 12;
+
+        }
+    }
+
+    private void WinCutscene()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.Append(ball.transform.DOMove(transform.position, 0.3f));
+    }
+}
