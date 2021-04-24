0bf8044685ee713730a51b9a17a0941c878067d9
RyanSalam <Ryan@agni.com>
2021-04-24 15:00:16 -0400

    Level Stuff

---
+++ Assets/Scripts/BallPhysics.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallPhysics : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float ballSpeed = 5.0f;
+
+    private RaycastHit groundHit;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 desiredRotation = new Vector3(-verticalInput, 0, horizontalInput);
+        if (desiredRotation.magnitude > Mathf.Epsilon)
+        {
+            if (Physics.Raycast(transform.position, Vector3.down, out groundHit, 0.85f))
+            {
+                Debug.Log("We are hitting the ground");
+                Vector3 planeProj = Vector3.ProjectOnPlane(rb.velocity, groundHit.normal);
+
+                Debug.DrawRay(rb.position, planeProj * ballSpeed);
+
+                rb.AddForce(planeProj.normalized * ballSpeed, ForceMode.Acceleration);
+            }
+        }
+    }
+}
