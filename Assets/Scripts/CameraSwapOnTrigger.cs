76d47a9849457aa80208fa0b3a15c6d271701c90
RyanSalam <Ryan@agni.com>
2021-04-24 18:58:47 -0400

    Level2 and Ball Fixes

---
+++ Assets/Scripts/CameraSwapOnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSwapOnTrigger : MonoBehaviour
+{
+    [SerializeField] private bool exitCameraOnTriggerExit = false;
+    [SerializeField] private CinemachineVirtualCamera lookAtCamera;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            lookAtCamera.Priority = 13;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && exitCameraOnTriggerExit)
+        {
+            lookAtCamera.Priority = 9;
+        }
+    }
+}
