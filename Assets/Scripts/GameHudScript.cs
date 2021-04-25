2a7928d97d05540c6a08902c95e9de09d22b2dc2
RyanSalam <Ryan@agni.com>
2021-04-25 15:43:44 -0400

    In Game Hud Base  and meta deletes

---
+++ Assets/Scripts/GameHudScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHudScript : MonoBehaviour
+{
+    [SerializeField] private string NextLevelName;
+
+    public void OnRestartButton()
+    {
+        string scene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(scene);
+    }
+
+    public void OnNextLevelButton()
+    {
+        SceneManager.LoadScene(NextLevelName);
+    }
+
+    public void OnQuitButton()
+    {
+
+    }
+}
