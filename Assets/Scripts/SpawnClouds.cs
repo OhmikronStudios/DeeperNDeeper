9df57931cec43a37d8ca6fe42a3b4bd470de5870
Kevin Vickers <dreammchine64@hotmail.com>
2021-04-26 20:39:55 -0400

    Cloud attempt and color fix

---
+++ Assets/Scripts/SpawnClouds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnClouds : MonoBehaviour
+{
+    public float spawnHeight;
+    public float heightVariation;
+    public float spawnArea;
+    public int cloudAmount;
+
+    public GameObject cloudObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cloudAmount; i++)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(transform.position.x - spawnArea, transform.position.x + spawnArea), Random.Range(spawnHeight - heightVariation, spawnHeight + heightVariation), Random.Range(transform.position.z - spawnArea, transform.position.z + spawnArea));
+            Instantiate(cloudObj, spawnPos, Quaternion.identity, this.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
